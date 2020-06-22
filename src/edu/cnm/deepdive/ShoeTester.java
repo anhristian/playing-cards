4db6777d551f76c7ad608c2bd8ade41336f47d1e
Anastasia Hristian <an.hristian@gmail.com>
2020-06-22 09:42:44 -0600

    Implemented Comparable and Comparator.

--- src/edu/cnm/deepdive/ShoeTester.java
+++ src/edu/cnm/deepdive/ShoeTester.java
@@ -1,7 +1,9 @@
 package edu.cnm.deepdive;
 
+import java.io.Reader;
 import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 public class ShoeTester {
@@ -12,6 +14,29 @@ public class ShoeTester {
     Shoe shoe = new Shoe(1);
     System.out.println(Arrays.toString(shoe.getCards()));
     shoe.shuffle(rng);
+    System.out.println(Arrays.toString(shoe.getCards()));
+    shoe.sort();
+    System.out.println(Arrays.toString(shoe.getCards()));
+   shoe.sort(new Comparator<Card>() {
+     @Override
+     public int compare(Card c1, Card c2) {
+       int comparision = c1.getSuit().compareTo(c2.getSuit());
+       //first if looks at the suit  ; second if looks at the ranks.
+       if (comparision == 0) {
+         comparision = c1.getRank().compareTo(c2.getRank());
+         if (comparision != 0) {
+           if(c1.getRank().equals(Rank.ACE)) {
+             comparision = 1;
+           } else if (c2.getRank().equals(Rank.ACE)) {
+             comparision = -1;
+           }
+         }
+
+       }
+
+       return comparision;
+     }
+   });
     System.out.println(Arrays.toString(shoe.getCards()));
   }
 
